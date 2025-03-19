b7ba430da72ae9de5f75fd2f4ef032e469fe89db
Saurabh Barde <saurabhbarde1989@gmail.com>
2025-03-19 08:57:33 +0530

    small changes

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,5 +1,14 @@
 const Service = () => {
-  return <div>Service</div>;
+  return (
+    <div className="w-screen h-auto flex flex-col justify-center items-center relative px-[24px] sm:px-[64px] py-[32px] sm:py-[64px] overflow-hidden gap-[20px]">
+      <span className="text-[28px] sm:text-[42px] font-semibold">
+        Our Services
+      </span>
+      <span className="text-wrap text-[#525252] text-center text-[16px] sm:text-[20px]">
+        We provide a varitey of heating and cooling solutions
+      </span>
+    </div>
+);
 };
 
 export default Service;
