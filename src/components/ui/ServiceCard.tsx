874ee6feb1c9e56f2796702b0ae8e418d299d8c0
Palash2911 <palashrambhia2@gmail.com>
2025-03-20 21:48:41 +1100

    Deployement Push

--- src/components/ui/ServiceCard.tsx
+++ src/components/ui/ServiceCard.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import { cn } from '../../lib/utils'
+import { cn } from "../../lib/utils";
 
-interface cardProps{
-    image:string,
-    title:string,
-    content:string,
-    classname:string
+interface cardProps {
+  image: string;
+  title: string;
+  content: string;
+  classname: string;
 }
 
-
-const ServiceCard = ({image, title , content,classname}:cardProps) => {
+const ServiceCard = ({ image, title, content, classname }: cardProps) => {
   return (
-    <div className='w-auto max-w-sm h-auto max-h-[500px] flex flex-col justify-center items-start px-[24px] py-[18px] border-[2px] rounded-[10px] gap-[16px] hover:scale-[1.02] ease-in-out duration-300'>
-        <span className="w-full rounded-[6px] flex justify-center items-center overflow-hidden">
-          <img src={image} alt="" className={cn('w-[400px] h-[300px] object-cover',classname)} />
-        </span>
-        <span className=' text-[17px] sm:text-[18px] font-medium'>
-            {title}
-        </span>
-        <div className='text-[14px] sm:text-[15px] text-wrap text-[#525252]'>
-            {content}
-        </div>
+    <div className="w-auto max-w-sm h-auto max-h-[500px] flex flex-col justify-center items-start px-[24px] py-[18px] border-[2px] rounded-[10px] gap-[16px] hover:scale-[1.02] ease-in-out duration-300">
+      <span className="w-full rounded-[6px] flex justify-center items-center overflow-hidden">
+        <img
+          src={image}
+          alt=""
+          className={cn("w-[400px] h-[300px] object-cover", classname)}
+        />
+      </span>
+      <span className=" text-[17px] sm:text-[18px] font-medium">{title}</span>
+      <div className="text-[14px] sm:text-[15px] text-wrap text-[#525252]">
+        {content}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ServiceCard
+export default ServiceCard;
